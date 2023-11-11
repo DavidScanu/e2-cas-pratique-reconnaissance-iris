3fb1871018e205a1f10cea496c45e6eda396d1f7
David Scanu <DavidScanu@users.noreply.github.com>
2023-11-11 15:50:26 +0000

    update app.py

--- streamlit-app/src/app.py
+++ streamlit-app/src/app.py
@@ -18,10 +18,10 @@ from sklearn.preprocessing import LabelEncoder
 
 # Fucntions
 def check_file_exists(file_path):
-  if os.path.exists(file_path):
-    return True
-  else:
-    return False
+    if os.path.exists(file_path):
+        return True
+    else:
+        return False
 
 
 # Variables 
@@ -33,7 +33,7 @@ img_dim = (img_height, img_width, img_channels)
 
 # Employees JSON
 with open("employees_info.json", 'r') as f:
-  employees_dict = json.load(f)
+    employees_dict = json.load(f)
 
 # LR Model
 lr_classifier_path = "./models/lr_classifier.h5"
@@ -41,29 +41,36 @@ lr_classifier_restored = tf.keras.models.load_model(lr_classifier_path)
 # LR Label Encoder
 lr_label_encoder_path = "./models/lr_label_encoder.pkl"
 with open(lr_label_encoder_path, 'rb') as f:
-  lr_label_encoder = pickle.load(f) # deserialize using load()
+    lr_label_encoder = pickle.load(f) # deserialize using load()
 
 
-def inference(image_file, label_encoder):
-  """
-  Fonction qui réalise une prédiction à partir d'un fichier image (ndarray ou PIL Image).
-  """
-  # Ajout d'une dimension car le modèle accepte une liste d'images cad un objet à 4 dimensions.
-  image_file_more_dims = np.expand_dims(image_file, axis=0)
-  # Inférence
-  pred = np.argmax(lr_classifier_restored.predict(image_file_more_dims), axis=1)[0]
-  # Désencodage de l'index de classe en classe (string)
-  pred_class_str = label_encoder.classes_[pred]
-  # Retourne une string
-  return pred_class_str
+# Prédiction Gauche-Droite
+def inference_lr(image_file, label_encoder):
+    """
+    Fonction qui réalise une prédiction à partir d'un fichier image (ndarray ou PIL Image).
+    """
+    # Ajout d'une dimension car le modèle accepte une liste d'images cad un objet à 4 dimensions.
+    image_file_more_dims = np.expand_dims(image_file, axis=0)
+    # Inférence
+    pred = np.argmax(lr_classifier_restored.predict(image_file_more_dims), axis=1)[0]
+    # Désencodage de l'index de classe en classe (string)
+    pred_class_str = label_encoder.classes_[pred]
+    # Retourne une string
+    return pred_class_str
+
+
+# Prédiction ID employé (oeil gauche)
+
+
+# prédiction ID employé (oeil droit)
 
 
 
 # Streamlit App
 st.set_page_config(
-  page_title="Reconnaissance d'iris",
-  page_icon="👀",
-  layout="centered"
+    page_title="Reconnaissance d'iris",
+    page_icon="👀",
+    layout="centered"
 )
 
 
@@ -75,30 +82,28 @@ st.divider()
 
 # List
 image = st.file_uploader(
-  "Choisissez une image",
-  type=['png', 'jpg', 'jpeg', 'bmp'],
-  accept_multiple_files=False,
-  label_visibility="visible"
+    "Choisissez une image",
+    type=['png', 'jpg', 'jpeg', 'bmp'],
+    accept_multiple_files=False,
+    label_visibility="visible"
 )
 if image is not None:
-  bytes_data = image.read() # bytes
-  image_pil = Image.open(io.BytesIO(bytes_data)) # PIL Object
-  st.image(image_pil)
+    bytes_data = image.read() # bytes
+    image_pil = Image.open(io.BytesIO(bytes_data)) # PIL Object
+    st.image(image_pil)
 
-  with st.spinner('Wait for it...'):
-    lr_pred = inference(image_pil, lr_label_encoder)
-  if lr_pred == 'left':
-    st.success('Left!')
-  elif lr_pred == 'right':
-    st.success('Right!')
+    with st.spinner('Wait for it...'):
+        lr_pred = inference_lr(image_pil, lr_label_encoder)
+        if lr_pred == 'left':
+            st.success('Left!')
+        elif lr_pred == 'right':
+            st.success('Right!')
 
-  # progress_text = "Operation in progress. Please wait."
-  # my_bar = st.progress(0, text=progress_text)
+    # progress_text = "Operation in progress. Please wait."
+    # my_bar = st.progress(0, text=progress_text)
 
-  # for percent_complete in range(100):
-  #   time.sleep(0.01)
-  #   my_bar.progress(percent_complete + 1, text=progress_text)
-  # time.sleep(1)
-  # my_bar.empty()
-
-
+    # for percent_complete in range(100):
+    #   time.sleep(0.01)
+    #   my_bar.progress(percent_complete + 1, text=progress_text)
+    # time.sleep(1)
+    # my_bar.empty()
