d851ce6263510d5e271b5e294d93b05b02462d4a
David Scanu <davidscanu14@gmail.com>
2023-12-08 16:30:05 +0100

    Update Dockerfile

--- streamlit-app/docker-run.sh
+++ streamlit-app/docker-run.sh
@@ -1,3 +1,3 @@
-docker run -d --name cas-pratique-streamlit-app -p 5002:5002 -v $(pwd)/src:/streamlit-app davidscanu/cas-pratique-streamlit-app
+docker run -it --name cas-pratique-streamlit-app -p 5002:5002 -v $(pwd)/src:/streamlit-app/src davidscanu/cas-pratique-streamlit-app
 
-docker run -it --name cas-pratique-streamlit-app -p 5002:5002 -v $(pwd)/src:/streamlit-app davidscanu/cas-pratique-streamlit-app
+docker run -d --name cas-pratique-streamlit-app -p 5002:5002 -v $(pwd)/src:/streamlit-app/src davidscanu/cas-pratique-streamlit-app
