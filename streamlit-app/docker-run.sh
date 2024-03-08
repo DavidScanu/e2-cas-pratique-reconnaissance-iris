a6df90e691243166811fc6d8f2da780236f1450c
David Scanu <DavidScanu@users.noreply.github.com>
2024-03-08 13:46:30 +0000

    Deleted docker-run.sh and added docker-compose.yaml

--- streamlit-app/docker-run.sh
+++
@@ -1,3 +0,0 @@
-docker run -it --name cas-pratique-streamlit-app -p 5002:5002 -v $(pwd)/src:/streamlit-app/src davidscanu/cas-pratique-streamlit-app
-
-docker run -d --name cas-pratique-streamlit-app -p 5002:5002 -v $(pwd)/src:/streamlit-app/src davidscanu/cas-pratique-streamlit-app
